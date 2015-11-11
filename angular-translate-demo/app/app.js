210a6bafaba2da2e8fba9a35b58addb49003c6dc
Hrvoje Hlevnjak <hrvoje.hlevnjak@axilis.hr>
2015-11-11 19:08:21 +0100

    Adding translations + translate service

--- angular-translate-demo/app/app.js
+++ angular-translate-demo/app/app.js
@@ -1,11 +1,23 @@
 var app = angular.module('translateApp', ['ui.router', 'ui.bootstrap', 'pascalprecht.translate'])
-.config(function () {
-	
+.config(function ($translateProvider) {
+	var translations = {
+		'TITLE': 'Welcome to the demo',
+		'DESCRIPTION': 'This is the angular-translate demo.'
+	};
+
+	$translateProvider
+		.translations('en', translations)
+		.preferredLanguage('en')
+		.fallbackLanguage('en');
 })
 .run(function ($translate) {
 
 })
-.controller('HomeController', function ($scope) {
-	$scope.text = "lalala";
+.controller('HomeController', function ($scope, $translate) {
+	$translate('TITLE').then(function (title) {
+		$scope.title = title;
+	});
+	$translate('DESCRIPTION').then(function (description) {
+		$scope.description = description;
+	});
 });
-;
