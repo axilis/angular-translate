114cd2b1454004e062318286c9f5cc5e66274fa9
Hrvoje Hlevnjak <hrvoje.hlevnjak@axilis.hr>
2015-11-11 18:39:35 +0100

    Initial app

---
+++ angular-translate-demo/app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('translateApp', ['ui.router', 'ui.bootstrap', 'pascalprecht.translate'])
+.config(function () {
+	
+})
+.run(function ($translate) {
+
+})
+.controller('HomeController', function ($scope) {
+	$scope.text = "lalala";
+});
+;
